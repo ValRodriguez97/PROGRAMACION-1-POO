a531f1d0048b2ee5af72498998d53a0e53ab96d6
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2024-04-22 13:48:51 -0500

    Fin tests

--- proyecto/src/test/java/co/edu/uniquindio/poo/Zonatest.java
+++ proyecto/src/test/java/co/edu/uniquindio/poo/Zonatest.java
@@ -23,4 +23,20 @@ public class Zonatest {
         assertEquals(3141592.65, zona.calcularValor(),0.05, App.PRECISION);
         LOG.info("Fin test valor zona circular con material arena");
     }
+
+    @Test
+    public void ValorZonaCuadradaGramaNatural (){
+        LOG.info("Inicio test valor total zona cuadrada con material grama natural");
+        Zona zona = new Zona("Zona 1", Material.GRAMA_NATURAL, new Cuadrado(10));
+        assertEquals(2000000.0, zona.calcularValor(), 0.05, App.PRECISION);
+        LOG.info("Inicio test valor total zona cuadrada con material grama natural");
+    }
+
+    @Test
+    public void ValorZonaTrianguloAsfalto (){
+        LOG.info("Inicio test valor total zona triangular con material asfalto");
+        Zona zona = new Zona("Zona 1", Material.ASFALTO, new Triangulo(10, 20));
+        assertEquals(4000000.0, zona.calcularValor(), 0.05, App.PRECISION);
+        LOG.info("Inicio test valor total zona triangular con material asfalto");
+    }
 }
